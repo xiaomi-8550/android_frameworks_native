1f383f72b5fa59c3e36c63fdee3d87868d1d009b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-12 23:25:57 +0530

    SF: Allow forcing HWC brightness support
    Some vendors purposely disable brightness capability
    even though the HWC completely supports it, in order
    to leverage their custom brightness implementations.
    One such example of this is in stock HWC of some
    xiaomi devices.
    
    Since light HAL offers lesser granual control than
    HWC (since its in gamma space and not float), and
    since it would be rather difficult to patch the blob
    to return the right capabilities, allow forcing the
    usage of HWC brightness functions via a prop:
    
    ro.sf.force_hwc_brightness=1 (default 0).
    
    Change-Id: I7d2fef03afbbb936572c717f769f30c9e49213e0
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -464,6 +464,9 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("ro.sf.force_light_brightness", value, "0");
     mForceLightBrightness = atoi(value);
 
+    property_get("ro.sf.force_hwc_brightness", value, "0");
+    mForceHwcBrightness = atoi(value);
+
     mIgnoreHwcPhysicalDisplayOrientation =
             base::GetBoolProperty("debug.sf.ignore_hwc_physical_display_orientation"s, false);
 
@@ -1944,11 +1947,9 @@ status_t SurfaceFlinger::getDisplayBrightnessSupport(const sp<IBinder>& displayT
     if (!displayId) {
         return NAME_NOT_FOUND;
     }
-    if (mForceLightBrightness) {
-        *outSupport = false;
-        return NO_ERROR;
-    }
-    *outSupport = getHwComposer().hasDisplayCapability(*displayId, DisplayCapability::BRIGHTNESS);
+
+    *outSupport = mForceHwcBrightness ? true : mForceLightBrightness ? false :
+            getHwComposer().hasDisplayCapability(*displayId, DisplayCapability::BRIGHTNESS);
     return NO_ERROR;
 }
 
--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1371,6 +1371,7 @@ private:
 
     bool mLumaSampling = true;
     bool mForceLightBrightness = false;
+    bool mForceHwcBrightness = false;
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;
